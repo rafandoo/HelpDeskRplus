729298177e617ae04ffefe835a5fcbb4cb396eee
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-10-15 21:27:21 -0300

    update contagem de tickets por status

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
                         <div class="row align-items-center no-gutters">
                             <div class="col me-2">
                                 <div class="text-uppercase text-danger fw-bold text-xs mb-1"><span>Aguardando Atendimento</span></div>
-                                <div class="text-dark fw-bold h5 mb-0"><span>AA</span></div>
+                                <div class="text-dark fw-bold h5 mb-0"><span>{{ $tickets->where('status_id', 1)->count() }}</span></div>
                             </div>
                             <div class="col-auto"><i class="fas fa-hourglass-half fa-2x text-gray-300"></i></div>
                         </div>
@@ -34,7 +34,7 @@
                         <div class="row align-items-center no-gutters">
                             <div class="col me-2">
                                 <div class="text-uppercase text-success fw-bold text-xs mb-1"><span>Em atendimento</span></div>
-                                <div class="text-dark fw-bold h5 mb-0"><span>EA</span></div>
+                                <div class="text-dark fw-bold h5 mb-0"><span>{{ $tickets->where('status_id', 2)->count() }}</span></div>
                             </div>
                             <div class="col-auto"><i class="fas fa-play fa-2x text-gray-300"></i></div>
                         </div>
@@ -49,7 +49,7 @@
                                 <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span>Pendentes</span></div>
                                 <div class="row g-0 align-items-center">
                                     <div class="col-auto">
-                                        <div class="text-dark fw-bold h5 mb-0"><span>PP</span></div>
+                                        <div class="text-dark fw-bold h5 mb-0"><span>{{ $tickets->where('status_id', 3)->count() }}</span></div>
                                     </div>
                                 </div>
                             </div>
@@ -58,19 +58,22 @@
                     </div>
                 </div>
             </a></div>
-        <div class="col-md-6 col-xl-3 mb-4"><a href="#" style="text-decoration: none;">
+        <div class="col-md-6 col-xl-3 mb-4">
+            <a href="#" style="text-decoration: none;">
                 <div class="card shadow border-start-primary py-2" style="text-decoration: none;">
                     <div class="card-body">
                         <div class="row align-items-center no-gutters">
                             <div class="col me-2">
-                                <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span class="text-info">finalizados</span></div>
-                                <div class="text-dark fw-bold h5 mb-0"><span>FF</span></div>
+                                <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span class="text-info">Finalizados</span></div>
+                                @php $data = date('Y-m-d'); @endphp
+                                <div class="text-dark fw-bold h5 mb-0"><span>{{ $tickets->where('status_id', 4)->where('closed_at', '>=', $data)->count() }}</span></div>
                             </div>
                             <div class="col-auto"><i class="fas fa-check-circle fa-2x text-gray-300"></i></div>
                         </div>
                     </div>
                 </div>
-            </a></div>
+            </a>
+        </div>
     </div>
     <div class="row" hidden>
         <div class="col-lg-6 mb-4">
