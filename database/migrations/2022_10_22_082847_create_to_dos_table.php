2151b763812d1a41ba614887b390e27f29315e90
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-10-22 08:35:27 -0300

    Model inicial

---
+++ database/migrations/2022_10_22_082847_create_to_dos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_dos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->time('time');
+            $table->date('date');
+            $table->boolean('completed')->default(false);
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_dos');
+    }
+};
