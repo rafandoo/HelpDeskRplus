e0627da43871a2a76309bd4160ba0d91da965006
rafandoo <rafaelcamargo.inf@gmail.com>
2022-11-11 15:16:27 -0300

    Factories tabelas simples

---
+++ database/factories/AccessLevelFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AccessLevel>
+ */
+class AccessLevelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->word,
+            'created_at' => $this->faker->dateTime, 
+            'updated_at' => $this->faker->dateTime
+        ];
+    }
+}
