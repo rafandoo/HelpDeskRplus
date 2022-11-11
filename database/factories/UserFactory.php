ba7252971e6e97d419aa795dece40f2cb6007863
rafandoo <rafaelcamargo.inf@gmail.com>
2022-11-11 16:57:03 -0300

    Factory/seeder de user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,18 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $access_level = \App\Models\AccessLevel::inRandomOrder()->first();
         return [
             'name' => fake()->name(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
+            'login' => fake()->unique()->userName(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'access_level' => $access_level->id,
             'remember_token' => Str::random(10),
+            'created_at' => fake()->dateTime(),
+            'updated_at' => fake()->dateTime()
         ];
     }
 
